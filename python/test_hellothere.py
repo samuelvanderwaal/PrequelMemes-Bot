85c0d76fc526cb902ff8fc2b54f9ba3ddc26550b
Samuel Vanderwaal <samuel.vanderwaal@gmail.com>
2018-09-06 11:15:08 -0800

    Refactors hellothere.py; adds beginning of tests

--- python/test_hellothere.py
+++ python/test_hellothere.py
@@ -4,15 +4,23 @@ import json
 import hellothere
 
 class Comment:
-	def __init__(self, author, body):
-		self.author = author
-		self.body = body
-		self.permalink = "/" + ''.join(random.choice(string.ascii_lowercase) for x in range(10))
+    def __init__(self, author, body):
+        self.author = author
+        self.body = body
+        self.permalink = "/" + ''.join(random.choice(string.ascii_lowercase) for x in range(10))
 
-	def reply(self, message):
-		print(message)
+    def reply(self, message):
+        print(message)
 
 
 def test_comments():
-	pass
+    test_comments_file = open("test_comments.json", "r")
+    test_comments = json.load(test_comments_file)
 
+    for test_comment in test_comments:
+        comment = Comment(test_comment["author"], test_comment["body"])
+
+        hellothere.process_submission(comment)
+
+
+test_comments()
